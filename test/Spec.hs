34c341a86ea1033a49823bc18d11b3095c71222b
Vanessa McHale <vamchale@gmail.com>
2021-12-24 17:05:52 -0500

    reasonable

--- test/Spec.hs
+++ test/Spec.hs
@@ -10,9 +10,7 @@ import           Test.Tasty.HUnit
 main :: IO ()
 main = defaultMain $
     testGroup "High-level interface"
-        [ testCase "Compiles a regex" (compileY ".*")
-        , testCase "Matches a file extension" (matchY "\\.awk$" "/usr/share/vim/vim82/tools/mve.awk")
-        , testCase "Matches a date" (matchY "^\\d{4}-\\d{2}-\\d{2}$" "2014-01-01")
+        [ testCase "Matches a file extension" (matchY "\\.awk$" "/usr/share/vim/vim82/tools/mve.awk")
         , testCase "Matches at" (findAt "libj\\.dylib$" "/Applications/j903/bin/libj.dylib" (RureMatch 23 33))
         , testCase "Matches at (plural)" $
             matchesAt "\\d{4}-\\d{2}-\\d{2}" "2012-03-14 2013-01-01 2014-07-05"
